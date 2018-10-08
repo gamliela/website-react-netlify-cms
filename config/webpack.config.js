367aaf973b2c5dd645ab25f09e188cdf9e762ec5
Alon Gamliel <alon@gamliela.com>
2018-10-08 23:53:43 +1300

    WIP: Add netlify-cms

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 // paths
 const projectPath = path.resolve(__dirname, '..');
 const buildPath = path.join(projectPath, 'public');
 const srcPath = path.join(projectPath, 'src');
 const appPath = path.join(srcPath, 'app');
+const adminPath = path.join(srcPath, 'admin');
+const mediaPath = path.join(projectPath, 'media');
 
 const config = {
-  entry: path.join(appPath, 'index.tsx'),
+  entry: {
+    'app': path.join(appPath, 'index.tsx'),
+    'admin/admin': path.join(adminPath, 'index.tsx')
+  },
   output: {
-    filename: 'bundle.js',
-    path: buildPath
+    path: buildPath,
+    filename: '[name].js'
   },
   module: {
     rules: [
@@ -53,8 +59,25 @@ const config = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(appPath, 'index.html')
-    })
+      template: path.join(appPath, 'index.html'),
+      chunks: ['app'],
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(adminPath, 'index.html'),
+      chunks: ['admin/admin'],
+      filename: 'admin/index.html'
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.join(adminPath, 'config.yml'),
+        to: 'admin/config.yml'
+      },
+      {
+        from: mediaPath,
+        to: path.join(buildPath, 'media')
+      }
+    ])
   ],
   devServer: {
     contentBase: buildPath
--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -1,1 +1,8 @@
-import CMS from 'netlify-cms';
+import 'netlify-cms';
+import * as netlifyIdentity from 'netlify-identity-widget';
+
+netlifyIdentity.init({
+  container: "body"
+});
+
+window['netlifyIdentity'] = netlifyIdentity;  // netlify cms expect this global
